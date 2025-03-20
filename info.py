f3d8974a2ce7683c6cc2dff460425066f35084aa
Oo <officialwildcash4@gmail.com>
2025-03-20 15:02:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 # This Channel Is For When User Start Your Bot Then Bot Send That User Name And Id In This Log Channel, Same For Group Also.
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', ''))
-BLOGSPOT_URL = environ.get('BLOGSPOT_URL', 'https://fjhhfwjhfhj.blogspot.com/2025/03/redirecting.html')
+BLOGSPOT_URL = environ.get('BLOGSPOT_URL', 'https://telegramopostredirect.blogspot.com')
 
 # This Is File Channel Where You Upload Your File Then Bot Automatically Save It In Database 
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]  # For Multiple Id Use One Space Between Each.
